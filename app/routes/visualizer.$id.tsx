7c05d3ab2a3728d15a2ca3a47850181c69f68bbf
James Green <jdossgreen@icloud.com>
2026-02-20 12:18:40 -0800

    host and upload images

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,7 +1,24 @@
-const VisualizerId = () => {
-    return (
-        <div>VisualizerId</div>
-    )
-}
+import React from "react";
+import { useLocation } from "react-router";
 
-export default VisualizerId
+const VisualizerId = () => {
+    const location = useLocation();
+    const { initialImage, name } = location.state || {};
+    const imageBlock = initialImage ? (
+        <div>
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="Source" />
+        </div>
+    ) : null;
+
+    return (
+        <section>
+            <h1>{name || 'Untitled Project'}</h1>
+            <div className="visualizer">
+                {imageBlock}
+            </div>
+        </section>
+    );
+};
+
+export default VisualizerId;
